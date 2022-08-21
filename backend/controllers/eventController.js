7450b06e4acb4791d25fe2531642aac46fbc4123
Dean <dinbiton100@gmail.com>
2022-08-21 17:03:13 +0300

    express async handler

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Events
 // @route GET /api/events
 // @access Public
-const getEvents = async (req, res) => {
+const getEvents = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get events'})
-}
+})
 
 // @desc Set Event
 // @route POST /api/events
 // @access Public
-const setEvents = async (req, res) => {
+const setEvents = asyncHandler(async (req, res) => {
     if(!req.body.text){
         //res.status(400).json({message: 'Please add a text field'})
         res.status(400)
@@ -16,21 +18,21 @@ const setEvents = async (req, res) => {
     }
 
     res.status(200).json({ message: 'Set event'})
-}
+})
 
 // @desc Update Event
 // @route PUT /api/events/:id
 // @access Public
-const updateEvents = async (req, res) => {
+const updateEvents = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update event ${req.params.id}`})
-}
+})
 
 // @desc Delete Events
 // @route DELETE /api/events/:id
 // @access Public
-const deleteEvents = async (req, res) => {
+const deleteEvents = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete event ${req.params.id}`})
-}
+})
 
 module.exports = {
     getEvents, setEvents, updateEvents, deleteEvents
