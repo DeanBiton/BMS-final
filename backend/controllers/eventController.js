affd073d051c51618323c9af2b5c9668766fbc11
Dean <dinbiton100@gmail.com>
2022-08-21 16:59:38 +0300

    error and exception handeling

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,16 +1,18 @@
 // @desc Get Events
 // @route GET /api/events
 // @access Public
-const getEvents = (req, res) => {
+const getEvents = async (req, res) => {
     res.status(200).json({ message: 'Get events'})
 }
 
 // @desc Set Event
 // @route POST /api/events
 // @access Public
-const setEvents = (req, res) => {
+const setEvents = async (req, res) => {
     if(!req.body.text){
-        res.status(400).json({message: 'Please add a text field'})
+        //res.status(400).json({message: 'Please add a text field'})
+        res.status(400)
+        throw new Error('Please add a text field')
     }
 
     res.status(200).json({ message: 'Set event'})
@@ -19,14 +21,14 @@ const setEvents = (req, res) => {
 // @desc Update Event
 // @route PUT /api/events/:id
 // @access Public
-const updateEvents = (req, res) => {
+const updateEvents = async (req, res) => {
     res.status(200).json({ message: `Update event ${req.params.id}`})
 }
 
 // @desc Delete Events
 // @route DELETE /api/events/:id
 // @access Public
-const deleteEvents = (req, res) => {
+const deleteEvents = async (req, res) => {
     res.status(200).json({ message: `Delete event ${req.params.id}`})
 }
 
