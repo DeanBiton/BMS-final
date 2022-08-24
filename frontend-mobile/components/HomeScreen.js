08379f4cb1fc421b0f80369beaed7dfc1d44063c
Dean <dinbiton100@gmail.com>
2022-08-24 20:12:56 +0300

    fix getGoals bug

--- frontend-mobile/components/HomeScreen.js
+++ frontend-mobile/components/HomeScreen.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Events from './EventsScreen';
+import { useSelector } from 'react-redux'
+
 
 function Home() {
+  const { user } = useSelector((state) => state.auth)
   const navigation = useNavigation();
   const handleLiveTranslateButtonClicked = () => {
     navigation.replace('Events');
@@ -11,12 +14,11 @@ function Home() {
   return (
     <View>
       <Text>The Home page</Text>
-      <Text>Welcome</Text>
+      <Text>Welcome {user && user.name}</Text>
       <Button
           onPress={handleLiveTranslateButtonClicked}
-          title="Events">
-          
-        </Button>
+          title="Events">    
+      </Button>
     </View>
   );
 }
