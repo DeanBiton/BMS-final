0e342b1ff695676b04fd8aadcb82d10517f6fbed
Dean <dinbiton100@gmail.com>
2022-09-05 17:03:18 +0300

    Custom Drawer

---
+++ frontend-mobile/components/NavigationComponents/AppDrawer.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import CustomDrawer from './CustomDrawer';
+import HomeScreen from '../HomeScreen';
+import EventsScreen from '../EventComponents/EventsScreen';
+const Drawer = createDrawerNavigator()
+
+function AppDrawer() {
+  const { user } = useSelector((state) => state.auth)
+  
+  useEffect(() => {
+  }, [user,])
+
+  return (
+      <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Events" component={EventsScreen} />
+      </Drawer.Navigator>
+  )
+}
+
+
+export default AppDrawer
