bf99d4cba0ca01303d682b06088e0295dc124f08
BelikovYoav <yoavbl@mta.ac.il>
2022-09-07 19:05:47 +0300

    temp

---
+++ frontend-web/src/components/UpdateDemand.jsx
@@ -0,0 +1,49 @@
+import Paper from '@mui/material/Paper';
+import Button from "@mui/material/Button";
+import BloodForm from '../components/CreateEvent/BloodForm'
+import * as React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { updateEvent } from '../features/events/eventSlice'
+
+
+export default function Test({eventId, data, handle}){
+    const dispatch = useDispatch()
+
+
+    const[formData,setFormData] = React.useState({
+        "id":eventId,
+        "A+":data["A+"],
+        "A-":data["A-"],
+        "B+":data["B+"],
+        "B-":data["B+"],
+        "AB+":data["AB+"],
+        "AB-":data["AB-"],
+        "O+":data["O+"],
+        "O-":data["O-"]
+    })
+
+    function handleChange(event) {
+        const {name, value,type} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: (type === 'number' && value < 0)? 0 : value
+            }
+        })
+    }
+
+    function handleOk(){
+        //console.log(formData)
+        dispatch(updateEvent(formData))
+        handle(false)
+        // window.location.reload(false);
+
+    }
+return(
+    <Paper  className="eventInnerPaper" variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+    <BloodForm formData={formData} handleFunc={handleChange}/>
+    <Button  variant="contained" onClick={handleOk}>Update</Button>
+    <Button  variant="contained" onClick={()=>handle(false)} >Cancel</Button>
+    </Paper>
+)
+}
--- frontend-web/src/pages/Event.jsx
+++ frontend-web/src/pages/Event.jsx
@@ -1,26 +1,62 @@
 import AddressForm from '../components/CreateEvent/AddressForm';
+import BloodForm from '../components/CreateEvent/BloodForm'
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
+import Button from "@mui/material/Button";
+import Popover from '@mui/material/Popover';
 import { useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import SimpleTable from "../components/Table"
+import { useState, useEffect } from 'react'
+import UpdateDemand from '../components/UpdateDemand'
 
 export default function Test(){
     const event = useLocation().state.event
     const dispatch = useDispatch()
-    console.log(event)
+    // console.log(event)
 
+    const { events, isLoading, isError, message } = useSelector(
+        (state) => state.events
+    )
+    
+    function handleDelete(){
+        // dispatch(deleteEvent(event._id))
+        console.log(event._id)
+    }
 
+    function updateDemand(){
+        console.log("update")
+        setShowDemand(false)
+    }
+    const [showDemand, setShowDemand] = useState(false)
         return(
             <Container component="main" maxWidth="m" sx={{ mb: 4 }}>
                         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
     
             <AddressForm formData={event} handleFunc={undefined} handleDateTimeFunc={undefined} isDisable={true} />
-            <SimpleTable className="eventTable"rows={[
-                {Type:"bloodTypeDonated",...event.bloodTypeDonated},
-                {Type:"BloodTypeRegisters",...event.bloodTypeRegisters},
-                {Type:"BloodTypeDemands",...event.bloodTypeDemands}]}/>
+            {!showDemand? (
+                    <>
+                    <div className='wrapperRightSide'> 
+                    <Button id='showDemand' variant="contained" onClick={()=>setShowDemand(true)} >Update Demand</Button>
+                    </div>
+                    <SimpleTable className="eventTable"rows={[
+                        {Type:"bloodTypeDonated",...event.bloodTypeDonated},
+                        {Type:"BloodTypeRegisters",...event.bloodTypeRegisters},
+                        {Type:"BloodTypeDemands",...event.bloodTypeDemands}]}/>
+                        </>
+            ) : (
+                <UpdateDemand  eventId ={event._id}data={event.bloodTypeDemands} handle={()=>setShowDemand(false)}/>
 
+            //     <Paper  className="eventInnerPaper" variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            //     <BloodForm formData={event.bloodTypeDemands} handleFunc={undefined}/>
+            //     <Button  variant="contained">Update</Button>
+            //     <Button  variant="contained" onClick={()=>setShowDemand(false)} >Cancel</Button>
+            // </Paper>
+            )}
+
+ 
+            
+                 <Button id='deleteEvent' variant="contained">Delete Event</Button>
             </Paper>
             </Container>
         )
